b1ee2def7470cee600077277d4a3693b18cf8102
vishal945 <76213800+vishal945@users.noreply.github.com>
2023-04-08 14:43:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restaurant from "./component/basics/Restaurant";
+ 
+
+const App = () => {
+  return (
+     <>
+        <Restaurant></Restaurant>
+     </>
+  )
+}
+
+export default App
